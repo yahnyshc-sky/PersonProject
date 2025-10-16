c56bd773e6c3dd0b2eed06d0103a06a11a2fdd35
Maks <yahnyshc.sky.personal@gmail.com>
2025-10-16 15:47:26 +0100

    CREATE: /hello and /helloyou endpoints.

--- src/main/java/com/qa/PersonProject/PersonProjectApplication.java
+++ src/main/java/com/qa/PersonProject/PersonProjectApplication.java
@@ -2,12 +2,26 @@ package com.qa.PersonProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PersonProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonProjectApplication.class, args);
 	}
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello world!";
+    }
+
+    @GetMapping("/helloyou")
+    public String sayHelloYou(@RequestParam(name = "firstname", defaultValue = "John") String firstname){
+        return String.format("Hello %s!", firstname);
+    }
 
 }
