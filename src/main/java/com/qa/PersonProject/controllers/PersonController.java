4a414d22eeb311f80b92d587fd3dba8d18a7c8cc
Daveraj <daveraj.nothay@sky.uk>
2025-10-16 16:10:38 +0100

    CREATE: getAll and create endpoints

--- src/main/java/com/qa/PersonProject/controllers/PersonController.java
+++ src/main/java/com/qa/PersonProject/controllers/PersonController.java
@@ -1,13 +1,32 @@
 package com.qa.PersonProject.controllers;
 
+import com.qa.PersonProject.entities.Person;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PersonController {
 
+    private List<Person> people = new ArrayList<>();
+
     @GetMapping("/health")
     public String health() {
         return "My health is ok";
+    }
+
+    @PostMapping("/create")
+    public boolean addPerson(@RequestBody @Valid Person person){
+        return this.people.add(person);
+    }
+
+    @GetMapping("/getAll")
+    public List<Person> getAll() {
+        return this.people;
     }
 }
