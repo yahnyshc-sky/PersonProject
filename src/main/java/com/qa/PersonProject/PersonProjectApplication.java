a760724d30188cad21227458ae5728b495eb439a
Maks <yahnyshc.sky.personal@gmail.com>
2025-10-16 15:32:34 +0100

    Init repo.

---
+++ src/main/java/com/qa/PersonProject/PersonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.PersonProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonProjectApplication.class, args);
+	}
+
+}
